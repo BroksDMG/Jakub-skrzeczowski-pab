2fed802c7911de73e5b65c5c6b0cf03a30520e0a
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-03-26 11:39:07 +0100

    lab-1 poprawki

--- index.ts
+++ index.ts
@@ -1,4 +1,40 @@
-const express = require('express')
-const app = express()
-app.get('/', function (req, res) { res.send('Hello World') })
+
+const express = require('express')  
+const app = express()  
+app.get('/', function (req, res) {  
+  res.send('Hello World')  
+})  
+
+app.get('/add/:num1/:num2', (req, res) => { 
+     let num1  = parseInt(req.params.num1)
+     let num2  = parseInt(req.params.num2)
+     
+     console.log("connection was made")
+    res.send(num1+" + "+num2+" = "+(num1+num2))
+     
+})
+app.get('/minus/:num1/:num2', (req, res) => { 
+    let num1  = parseInt(req.params.num1)
+    let num2  = parseInt(req.params.num2)
+    
+    console.log("connection was made")
+   res.send(num1+" + "+num2+" = "+(num1-num2))
+    
+})
+app.get('/multiply/:num1/:num2', (req, res) => { 
+    let num1  = parseInt(req.params.num1)
+    let num2  = parseInt(req.params.num2)
+    
+    console.log("connection was made")
+   res.send(num1+" + "+num2+" = "+(num1*num2))
+    
+})
+app.get('/division/:num1/:num2', (req, res) => { 
+    let num1  = parseInt(req.params.num1)
+    let num2  = parseInt(req.params.num2)
+    
+    console.log("connection was made")
+   res.send(num1+" + "+num2+" = "+(num1/num2))
+    
+})
 app.listen(3000)
